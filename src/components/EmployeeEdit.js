d75ed7dd69bc973bff0bed8bba483feecb46a56d
Gopi Raja <gopi@inkmonk.com>
2017-04-22 21:27:13 +0530

    seems done

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,11 +2,13 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component{
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -24,6 +26,17 @@ class EmployeeEdit extends Component{
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  onAccept(){
+    const { uid } = this.props.employee;
+    
+    this.props.employeeDelete({ uid });
+  }
+
+  onDecline(){
+    this.setState({ showModal: false });
+  }
+
+
   render(){
     return(
       <Card>
@@ -38,6 +51,20 @@ class EmployeeEdit extends Component{
             Text Schdule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure want to delete this?
+        </Confirm>
       </Card>
     );
   }
@@ -49,4 +76,6 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, {employeeUpdate, employeeSave})(EmployeeEdit);
+export default connect(mapStateToProps, {
+  employeeUpdate, employeeSave, employeeDelete
+})(EmployeeEdit);
